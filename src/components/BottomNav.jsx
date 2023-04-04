7596281f0729571b7b3952e9ff4c30bcbae79b89
David Umoru <umorudavido@gmail.com>
2023-04-04 13:40:45 +0000

    Adjust bottom navbar color and sizing

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -12,42 +12,42 @@ const BottomNav = () => {
 			<div className="flex justify-between">
 				<a
 					href="#"
-					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center pt-2 pb-1"
+					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center"
 				>
-					<img src={homeIcon} alt="home icon" className="w-10 h-auto nav-icon" />
-					<span className="tab block text-xs text-white">Home</span>
+					<img src={homeIcon} alt="home icon" className="w-auto h-8 nav-icon" />
+					<span className="tab block text-xs text-white py-1">Home</span>
 				</a>
 
 				<a
 					href="#"
-					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center pt-2 pb-1"
+					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center"
 				>
-					<img src={gameIcon} alt="games icon" className="w-10 h-auto nav-icon" />
-					<span className="tab block text-xs text-white">Games</span>
+					<img src={gameIcon} alt="games icon" className="w-auto h-8 nav-icon" />
+					<span className="tab block text-xs text-white py-1">Games</span>
 				</a>
 
 				<a
 					href="#"
-					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center pt-2 pb-1"
+					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center"
 				>
-					<img src={rankingIcon} alt="ranking icon" className="w-10 h-auto nav-icon" />
-					<span className="tab block text-xs text-white">Leaderboard</span>
+					<img src={rankingIcon} alt="ranking icon" className="w-auto h-8 nav-icon" />
+					<span className="tab block text-xs text-white py-1">Leaderboard</span>
 				</a>
 
 				<a
 					href="#"
-					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center pt-2 pb-1"
+					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center"
 				>
-					<img src={walletIcon} alt="wallet icon" className="w-10 h-auto nav-icon" />
-					<span className="tab block text-xs text-white">Wallet</span>
+					<img src={walletIcon} alt="wallet icon" className="w-auto h-8 nav-icon" />
+					<span className="tab block text-xs text-white py-1">Wallet</span>
 				</a>
 
 				<a
 					href="#"
-					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center pt-2 pb-1"
+					className="w-full focus:text-teal-500 hover:text-teal-500 flex flex-col items-center justify-center"
 				>
-					<img src={accountIcon} alt="account icon" className="w-10 h-auto nav-icon" />
-					<span className="tab block text-xs text-white">Account</span>
+					<img src={accountIcon} alt="account icon" className="w-auto h-8 nav-icon" />
+					<span className="tab block text-xs text-white py-1">Account</span>
 				</a>
 			</div>
     	</div>
