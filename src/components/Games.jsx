24334d82926812a9f159e070270c353ada5e8e88
David Umoru <umorudavido@gmail.com>
2023-04-27 00:25:13 +0100

    Add Games Page and GET info from API

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+
 import GameTabs from "./GameTabs"
+import GameCards from "./GameCards";
 
 const Games = () => {
   return (
     <>
       <GameTabs />
       <div>Games</div>
+      <GameCards />
     </>
   );
 };
