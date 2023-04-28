62593630a2dbe70bf55c7cae19e68c136da6819d
David Umoru <umorudavido@gmail.com>
2023-04-28 14:38:00 +0100

    Display game content in game room

--- src/components/GameRoom.jsx
+++ src/components/GameRoom.jsx
@@ -1,43 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
 
 function GameRoom() {
-  const [games, setGames] = useState([]);
+  const [game, setGame] = useState(null);
+  const location = useLocation();
+  const gameId = new URLSearchParams(location.search).get("id");
 
   useEffect(() => {
     axios
-      .get('https://staging-api.awujo.gg/api/v1/game-room')
+      .get(`https://staging-api.awujo.gg/api/v1/create-game/${gameId}`)
       .then((response) => {
-        setGames(response.data);
+        setGame(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [gameId]);
+
+  if (!game) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>Game Room</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Game Title</th>
-            <th>Publisher</th>
-            <th>Platform</th>
-            <th>Players</th>
-          </tr>
-        </thead>
-        <tbody>
-          {games.map((game) => (
-            <tr key={game.id}>
-              <td>{game.gameTitle}</td>
-              <td>{game.publisher}</td>
-              <td>{game.platform}</td>
-              <td>{game.players}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Card className="bg-gray-900 w-[100rem] mx-auto my-8">
+        <CardHeader floated={false} className="h-[30rem]">
+          <img
+            src={game.gameFlyer}
+            alt="game flyer"
+            className="object-cover h-full w-full"
+          />
+        </CardHeader>
+        <CardBody className="text-center">
+          <Typography variant="h4" color="white" className="mb-2">
+            {game.gameTitle}
+          </Typography>
+          <Typography color="gray" className="font-medium" textGradient>
+            {game.description}
+          </Typography>
+          <Typography color="gray" className="font-medium" textGradient>
+            Platform: {game.platform}
+          </Typography>
+          <Typography color="gray" className="font-medium" textGradient>
+            {game.players} Players
+          </Typography>
+          <Typography color="gray" className="font-medium" textGradient>
+            Published By: {game.publisher}
+          </Typography>
+        </CardBody>
+      </Card>
     </div>
   );
 }
